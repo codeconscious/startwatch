268d50564e582d9882d52d86f8865440f4aaf8bf
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-12-25 21:43:26 +0900

    Refactor formatting not to be an extension method

--- Startwatch.Library/Library.fs
+++ Startwatch.Library/Library.fs
@@ -3,55 +3,51 @@
 open System
 open System.Diagnostics
 
-module Extensions =
-    type TimeSpan with
-        member this.ElapsedFriendly() =
-            match this with
-            | t when t.Ticks < 0 -> raise <| NotSupportedException("Negative TimeSpans are currently not supported.")
-            | t when t.Ticks = 0 -> "no time"
-            | t when t.TotalMilliseconds < 1 -> sprintf "%s" (t.TotalNanoseconds.ToString("#,##0ns"))
-            | t when t.TotalMilliseconds < 1000 -> sprintf "%s" (t.TotalMilliseconds.ToString("#,##0ms"))
-            | t ->
-                let days = t.Days
-                let hours = t.Hours
-                let mins = t.Minutes
-                let secs = t.Seconds
+module Logic =
+    let format (timespan: TimeSpan) =
+        match timespan with
+        | t when t.Ticks < 0 -> raise <| NotSupportedException("Negative TimeSpans are currently not supported.")
+        | t when t.Ticks = 0 -> "no time"
+        | t when t.TotalMilliseconds < 1 -> sprintf "%s" (t.TotalNanoseconds.ToString("#,##0ns"))
+        | t when t.TotalMilliseconds < 1000 -> sprintf "%s" (t.TotalMilliseconds.ToString("#,##0ms"))
+        | t ->
+            let days = t.Days
+            let hours = t.Hours
+            let mins = t.Minutes
+            let secs = t.Seconds
 
-                let prependText = if secs = 0 then "exactly " else String.Empty
+            let prependText = if secs = 0 then "exactly " else String.Empty
 
-                let hourText =
-                    match (days, hours) with
-                    | d, _ when d > 0 -> sprintf "%s" ((hours + (days * 24)).ToString("#,##0h"))
-                    | _, h when h > 0 -> sprintf "%s" ((hours + (days * 24)).ToString("#,##0h"))
-                    | _ -> String.Empty
+            let hourText =
+                match (days, hours) with
+                | d, _ when d > 0 -> sprintf "%s" ((hours + (days * 24)).ToString("#,##0h"))
+                | _, h when h > 0 -> sprintf "%s" ((hours + (days * 24)).ToString("#,##0h"))
+                | _ -> String.Empty
 
-                let minText =
-                    match (hours, mins) with
-                    | h, m when h > 0 && m > 0 -> sprintf "%s" (m.ToString("00m"))
-                    | _, m when m > 0 -> sprintf "%s" (m.ToString("0m"))
-                    | _ -> String.Empty
+            let minText =
+                match (hours, mins) with
+                | h, m when h > 0 && m > 0 -> sprintf "%s" (m.ToString("00m"))
+                | _, m when m > 0 -> sprintf "%s" (m.ToString("0m"))
+                | _ -> String.Empty
 
-                let secText =
-                    match (days, hours, mins, secs) with
-                    | d, _, 0, s when d > 0 && s > 0 -> sprintf "%ss" (t.ToString("ss"))
-                    | _, 0, 0, s when s > 0 -> sprintf "%ss" (t.ToString("s\\.ff"))
-                    | _, h, _, s when h > 0 && s > 0 -> sprintf "%ss" (t.ToString("ss"))
-                    | _, _, m, s when m > 0 && s > 0 -> sprintf "%ss" (t.ToString("ss"))
-                    | _, _, _, s when s > 0 -> sprintf "%s" (t.ToString("s"))
-                    | _ -> String.Empty
+            let secText =
+                match (days, hours, mins, secs) with
+                | d, _, 0, s when d > 0 && s > 0 -> sprintf "%ss" (t.ToString("ss"))
+                | _, 0, 0, s when s > 0 -> sprintf "%ss" (t.ToString("s\\.ff"))
+                | _, h, _, s when h > 0 && s > 0 -> sprintf "%ss" (t.ToString("ss"))
+                | _, _, m, s when m > 0 && s > 0 -> sprintf "%ss" (t.ToString("ss"))
+                | _, _, _, s when s > 0 -> sprintf "%s" (t.ToString("s"))
+                | _ -> String.Empty
 
-                $"{prependText}{hourText}{minText}{secText}"
+            $"{prependText}{hourText}{minText}{secText}"
 
-open Extensions
+open Logic
 
 type Watch() =
-
     let mutable startedAt = Stopwatch.GetTimestamp()
 
     member _.ElapsedFriendly =
-        Stopwatch
-            .GetElapsedTime(startedAt)
-            .ElapsedFriendly()
+        format <| Stopwatch.GetElapsedTime(startedAt)
 
     member _.Restart =
         startedAt <- Stopwatch.GetTimestamp()
--- Startwatch.Tests/Tests.fs
+++ Startwatch.Tests/Tests.fs
@@ -2,59 +2,59 @@
 
 open System
 open Xunit
-open Startwatch.Library.Extensions
+open Startwatch.Library.Logic
 
 [<Fact>]
 let ``Throws for negative TimeSpans`` () =
     let timeSpan = TimeSpan.FromTicks(-7)
-    Assert.Throws<NotSupportedException>(fun _ -> timeSpan.ElapsedFriendly() :> obj)
+    Assert.Throws<NotSupportedException>(fun _ -> format timeSpan :> obj)
 
 [<Fact>]
-let ``Zero timespan`` () =
+let ``Zero timespans`` () =
     let timeSpan = TimeSpan.Zero // 1 tick == 100 nanoseconds
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "no time"
     Assert.Equal(expected, actual)
 
 [<Fact>]
-let ``Nanoseconds with 3 digits`` () =
+let ``Three-digit nanoseconds`` () =
     let timeSpan = TimeSpan.FromTicks(1) // 1 tick == 100 nanoseconds
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "100ns"
     Assert.Equal(expected, actual)
 
 [<Fact>]
-let ``Nanoseconds with 5 digits`` () =
+let ``Five-digit nanoseconds`` () =
     let timeSpan = TimeSpan.FromTicks(100L) // 1 tick == 100 nanoseconds
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "10,000ns"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit milliseconds`` () =
     let timeSpan = TimeSpan.FromMilliseconds(1.0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "1ms"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Double-digit milliseconds`` () =
     let timeSpan = TimeSpan.FromMilliseconds(99.0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "99ms"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Double-digit milliseconds with a small decimal`` () =
     let timeSpan = TimeSpan.FromMilliseconds(99.2)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "99ms"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Double-digit milliseconds with a large decimal`` () =
     let timeSpan = TimeSpan.FromMilliseconds(99.9)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "100ms"
     Assert.Equal(expected, actual)
 
@@ -62,194 +62,194 @@ let ``Double-digit milliseconds with a large decimal`` () =
 [<Fact>]
 let ``Triple-digit milliseconds`` () =
     let timeSpan = TimeSpan.FromMilliseconds(999.0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "999ms"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Triple-digit milliseconds with a small decimal`` () =
     let timeSpan = TimeSpan.FromMilliseconds(999.3)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "999ms"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Triple-digit milliseconds with a large decimal`` () =
     let timeSpan = TimeSpan.FromMilliseconds(999.9)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "1,000ms"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit seconds`` () =
     let timeSpan = TimeSpan(0, 0, 3)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "3.00s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit seconds with 500 milliseconds`` () =
     let timeSpan = TimeSpan(0, 0, 0, 3, 500)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "3.50s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit seconds with 520 milliseconds`` () =
     let timeSpan = TimeSpan(0, 0, 0, 3, 520)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "3.52s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit minute with no seconds`` () =
     let timeSpan = TimeSpan(0, 1, 0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "exactly 1m"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit minute with single-digit seconds`` () =
     let timeSpan = TimeSpan(0, 1, 5)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "1m05s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit minute with double-digit seconds`` () =
     let timeSpan = TimeSpan(0, 1, 30)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "1m30s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit minute with maximum seconds`` () =
     let timeSpan = TimeSpan(0, 1, 59)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "1m59s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Two-digit minutes with no seconds`` () =
     let timeSpan = TimeSpan(0, 59, 0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "exactly 59m"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Two-digit minutes with 30 seconds`` () =
     let timeSpan = TimeSpan(0, 59, 30)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "59m30s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit hour with no minutes or seconds`` () =
     let timeSpan = TimeSpan(7, 0, 0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "exactly 7h"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit hour with single-digit minutes but no seconds`` () =
     let timeSpan = TimeSpan(7, 7, 0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "exactly 7h07m"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit hour with single-digit minutes and single-digit seconds`` () =
     let timeSpan = TimeSpan(7, 7, 4)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "7h07m04s"
     Assert.Equal(expected, actual)
 
 let ``Single-digit hour with double-digit minutes but no seconds`` () =
     let timeSpan = TimeSpan(7, 20, 0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "exactly 7h20m"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit hour without minutes but with seconds`` () =
     let timeSpan = TimeSpan(7, 0, 47)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "7h47s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Double-digit hour with no minutes or seconds`` () =
     let timeSpan = TimeSpan(13, 0, 0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "exactly 13h"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit hour with single-digit minutes and with seconds`` () =
     let timeSpan = TimeSpan(1, 3, 8)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "1h03m08s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit hour with double-digit minutes and with seconds`` () =
     let timeSpan = TimeSpan(1, 55, 8)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "1h55m08s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Double-digit hour with single-digit minutes and with seconds`` () =
     let timeSpan = TimeSpan(12, 5, 8)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "12h05m08s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Double-digit hour over 24 hours with maximum minutes and seconds`` () =
     let timeSpan = TimeSpan(36, 59, 59)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "36h59m59s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit day with no minutes, hours, or seconds`` () =
     let timeSpan = TimeSpan(10, 0, 0, 0)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "exactly 240h"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit day with no minutes, hours, but with single-digit seconds`` () =
     let timeSpan = TimeSpan(10, 0, 0, 2)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "240h02s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit day with no hours, but with single-digit minutes seconds`` () =
     let timeSpan = TimeSpan(10, 0, 0, 2)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "240h02s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Single-digit day with minutes, hours, and seconds`` () =
     let timeSpan = TimeSpan(4, 4, 59, 59)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "100h59m59s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Triple-digit day with minutes, hours, and seconds`` () =
     let timeSpan = TimeSpan(100, 23, 59, 59)
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "2,423h59m59s"
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``Maximum TimeSpan`` () =
     let timeSpan = TimeSpan.MaxValue
-    let actual = timeSpan.ElapsedFriendly()
+    let actual = format timeSpan
     let expected = "256,204,778h48m05s"
     Assert.Equal(expected, actual)
